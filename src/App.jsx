c2bd32acfa5f1a35e17d033b85762906fe056a27
theringsofsaturn <emiliankasemi@gmail.com>
2021-10-31 20:53:02 +0100

    Login page component done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import "./app.scss";
 import Home from "./pages/home/Home";
 import Watch from "./pages/watch/Watch";
-import "./app.scss";
 import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 
 const App = () => {
-  return <Register />;
+  return <Login />;
 };
 
 export default App;
