5c73bcc48fbb7d8bc0affbaaa47f3115f4a0686f
theringsofsaturn <emiliankasemi@gmail.com>
2021-10-31 12:53:01 +0100

    added isHovered hooks

--- src/components/listitem/ListItem.jsx
+++ src/components/listitem/ListItem.jsx
@@ -1,29 +1,36 @@
 import "./listItem.scss";
+import "../list/List"
+import { useState } from "react";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
-import AddIcon from '@mui/icons-material/Add';
-import ThumbUpAltOutlinedIcon from '@mui/icons-material/ThumbUpAltOutlined';
-import ThumbDownAltOutlinedIcon from '@mui/icons-material/ThumbDownAltOutlined';
+import AddIcon from "@mui/icons-material/Add";
+import ThumbUpAltOutlinedIcon from "@mui/icons-material/ThumbUpAltOutlined";
+import ThumbDownAltOutlinedIcon from "@mui/icons-material/ThumbDownAltOutlined";
 
-const ListItem = () => {
+const ListItem = ({index}) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className="listItem">
-      <img
-        src="https://i.ytimg.com/vi/7L6BheyqeaM/maxresdefault.jpg"
-        alt=""
-      />
+    <div
+      className="listItem"
+      style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <img src="https://i.ytimg.com/vi/7L6BheyqeaM/maxresdefault.jpg" alt="" />
       <div className="itemInfo">
         <div className="icons">
           <PlayArrowIcon />
-          <AddIcon/>
-          <ThumbUpAltOutlinedIcon/>
-          <ThumbDownAltOutlinedIcon/>
+          <AddIcon />
+          <ThumbUpAltOutlinedIcon />
+          <ThumbDownAltOutlinedIcon />
         </div>
         <div className="itemInfoTop">
           <span>1 hour 36 mins</span>
           <span className="limit">+16</span>
           <span>1999</span>
           <div className="desc">
-            lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip
+            lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit
+            amet, consectetur adip
           </div>
           <div className="genre">Action</div>
         </div>
