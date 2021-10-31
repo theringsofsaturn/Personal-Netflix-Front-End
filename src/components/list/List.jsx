ae446f5b4d3a32359734b801d5e1ff8d4a6e478a
theringsofsaturn <emiliankasemi@gmail.com>
2021-10-31 02:57:59 +0100

    added list comp

---
+++ src/components/list/List.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const List = () => {
+    return (
+        
+        <div className="list">
+            List
+        </div>
+    )
+}
+
+export default List
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -2,12 +2,17 @@ import "./home.scss";
 import ProfilePicture from "../assets/profile_picture.jpg";
 import Navbar from "../components/navbar/Navbar";
 import Featured from "../components/featured/Featured";
+import List from "../components/list/List"
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
       <Featured/>
+      <List/>
+      <List/>
+      <List/>
+      <List/>
     </div>
   );
 };
