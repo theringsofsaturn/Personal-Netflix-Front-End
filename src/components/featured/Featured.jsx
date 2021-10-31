cfd3107676d34866df6169f8277ebdfb340df13c
theringsofsaturn <emiliankasemi@gmail.com>
2021-10-31 02:19:29 +0100

    Featured styles and condition
    added styles and condition for featured.
     If there is a type (like series or movies), we are gonna see these information also with options

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,32 +1,61 @@
-import "./featured.scss"
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import "./featured.scss";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 
-function Featured() {
-    return (
-        <div className="featured">
-            <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
-
-            <div className="info">
-                <img src="https://occ-0-1432-1433.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUZdeG1DrMstq-YKHZ-dA-cx2uQN_YbCYx7RABDk0y7F8ZK6nzgCz4bp5qJVgMizPbVpIvXrd4xMBQAuNe0xmuW2WjoeGMDn1cFO.webp?r=df1" alt="" />
-
-                <span className="desc">
-                    lorem ipsum dolor sit amet, consectetur adip
-                    lorem ipsum dolor sit amet, consectet
-                </span>
-                <div className="buttons">
-                    <button className="play">
-                    <PlayArrowIcon/>
-                    <span>Play</span>
-                    </button>
-                    <button className="more">
-                    <InfoOutlinedIcon/>
-                    <span>Info</span>
-                    </button>
-                </div>
-            </div>
+function Featured({ type }) {
+  return (
+    <div className="featured">
+      {/* Featured is receiving a prop {type}. If there is a type (like series or movies), we are gonna see these information also */}
+      {type && (
+        <div className="category">
+          {/* if type is movie our title will be "Movies", if it's not, it's gonna be "Series" */}
+          <span>{type === "movie" ? "Movies" : "Series"}</span>
+          <select name="genre" id="genre">
+            <option>Genre</option>
+            <option value="adventure">Adventure</option>
+            <option value="comedy">Comedy</option>
+            <option value="crime">Crime</option>
+            <option value="fantasy">Fantasy</option>
+            <option value="historical">Historical</option>
+            <option value="horror">Horror</option>
+            <option value="romance">Romance</option>
+            <option value="sci-fi">Sci-fi</option>
+            <option value="thriller">Thriller</option>
+            <option value="western">Western</option>
+            <option value="animation">Animation</option>
+            <option value="drama">Drama</option>
+            <option value="documentary">Documentary</option>
+          </select>
         </div>
-    )
+      )}
+      <img
+        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+      />
+
+      <div className="info">
+        <img
+          src="https://occ-0-1432-1433.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUZdeG1DrMstq-YKHZ-dA-cx2uQN_YbCYx7RABDk0y7F8ZK6nzgCz4bp5qJVgMizPbVpIvXrd4xMBQAuNe0xmuW2WjoeGMDn1cFO.webp?r=df1"
+          alt=""
+        />
+
+        <span className="desc">
+          lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit
+          amet, consectet
+        </span>
+        <div className="buttons">
+          <button className="play">
+            <PlayArrowIcon />
+            <span>Play</span>
+          </button>
+          <button className="more">
+            <InfoOutlinedIcon />
+            <span>Info</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Featured
+export default Featured;
