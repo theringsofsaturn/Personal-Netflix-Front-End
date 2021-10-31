da0ef145f4ec00a2de31f2c7c97d0cfbeec1459f
theringsofsaturn <emiliankasemi@gmail.com>
2021-10-31 04:24:33 +0100

    states and function for arrows
    now the arrows can slide left and right correctly

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,26 +1,38 @@
 import "./list.scss";
 import ListItem from "../listItem/ListItem";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import ArrowBackIosNewOutlinedIcon from "@mui/icons-material/ArrowBackIosNewOutlined";
 import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
 
 const List = () => {
+  const [isMoved, setIsMoved] = useState(false);
+  const [slideNumber, setSlideNumber] = useState(0);
+
+  // to select and get the refernce of the elements we use ref hook, useRef() does the same thing as querySelector or getElementById. But since we cannot use vanilla Javascript here...
   const listRef = useRef();
 
   // function that handles the arrows to slide 230px when clicked
   const handleClick = (direction) => {
-    // to select and get the refernce of the elements we use ref hook, useRef() does the same thing as querySelector or getElementById. But since we cannot use vanilla Javascript here...
-    if (direction === "left") {
-      listRef.current.style.transform = `translateX(230px)`;
+    setIsMoved(true);
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if (direction === "left" && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1);
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+    }
+
+    if (direction === "right" && slideNumber < 5) {
+      setSlideNumber(slideNumber + 1);
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
     }
   };
+
   return (
     <div className="list">
       <span className="listTitle">Continue to watch</span>
       <div className="wrapper">
         <ArrowBackIosNewOutlinedIcon
           className="sliderArrow left"
-          onClick={() => handleClick("left")} // on click function for arrow to slide 230 px left
+          onClick={() => handleClick("left")} // on click function for arrow to slide 230 px left and right
         />
         <div className="container" ref={listRef}>
           <ListItem />
@@ -43,7 +55,7 @@ const List = () => {
         </div>
         <ArrowForwardIosOutlinedIcon
           className="sliderArrow right"
-          onClick={() => handleClick("right")} // on click function for arrow to slide 230 px right
+          onClick={() => handleClick("right")} // on click function for arrow to slide 230 px left and right
         />
       </div>
     </div>
